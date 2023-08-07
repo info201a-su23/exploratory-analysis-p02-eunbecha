09e251ee51dabe732263d678dc870fe3feae7e0e
eunbecha <95003795+eunbecha@users.noreply.github.com>
2023-08-06 19:39:47 -0700

    added a description of chart

--- chart1.R
+++ chart1.R
@@ -1,10 +1,9 @@
 # Compares the country's life expectancy to schooling in 2015
-
 country_2015 <- data_life %>%
   filter(Year == 2015)
 
 chart_1 <- ggplot(country_2015, aes(x = Schooling, y = Life.expectancy)) +
   geom_point() +
-  labs(title = "Years of Schooling vs. Life Expectancy in 2015",
+  labs(title = "Years of Schooling Versus Life Expectancy in 2015",
        x = "Years of Schooling", y = "Life Expectancy") +
   theme_minimal()
