4e459250349ffa3af0068e53ea7243a7d3bc133f
nickand3 <nickand3@uw.edu>
2023-08-07 11:05:08 -0700

    fixed an error

--- chart3.R
+++ chart3.R
@@ -3,7 +3,7 @@ top_countries <- country_2015 %>%
   filter(Country %in% c("Slovenia", "Denmark", "Chile", "Cyprus", "Japan"))
 
 # Plot the scatter plot
-chart_3 <- ggplot(top_countries, aes(x = GDP, y = `Life expectancy`)) +
+chart_3 <- ggplot(top_countries, aes(x = GDP, y = Life.expectancy)) +
   geom_point(aes(color = Country), size = 3) +
   geom_smooth(method = "lm", se = FALSE, color = "black", linetype = "dashed") +
   labs(title = "GDP vs Life Expectancy in 2015 for Countries with Highest Life Expectancy",
